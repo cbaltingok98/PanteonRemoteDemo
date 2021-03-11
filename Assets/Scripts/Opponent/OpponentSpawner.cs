eb0b77d4446e4987939af5e4dfed30bafa8cf45b
Celik Berk Altingok <cbaltingok@gmail.com>
2021-03-12 01:12:15 +0300

    update

--- Assets/Scripts/Opponent/OpponentSpawner.cs
+++ Assets/Scripts/Opponent/OpponentSpawner.cs
@@ -5,24 +5,11 @@ public class OpponentSpawner : MonoBehaviour
     [SerializeField] private GameObject opponent;
     [SerializeField] private int size;
 
-    private Vector3 _spawnPos;
-    private float _xPos;
-    private float _increment;
-
-    private void Awake()
-    {
-        _xPos = -5f;
-        _increment = 10f / size;
-    }
-
     private void Start()
     {
-        _spawnPos = transform.position;
         for (var i = 0; i < size; i++)
         {
-            _spawnPos.x = _xPos;
-            _xPos += _increment;
-            Instantiate(opponent, _spawnPos, Quaternion.identity);   
+            Instantiate(opponent, transform.position, Quaternion.identity);
         }
     }
 }
